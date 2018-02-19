c20c977e8f99d2cc4c339bac5883d78a44f48619
RondelezLaura <laura.rondelez@student.howest.be>
2018-02-19 11:28:53 +0100

    vb print via homecontroller

---
+++ Les2DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Les2DemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string name)
+        {
+            return $"Ugh {name}";
+        }
+    }
+}
